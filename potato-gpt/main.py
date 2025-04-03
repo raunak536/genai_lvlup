08993fe1764854515b608ae5681f29436b557910
Raunak <raunakkaushik@gmail.com>
2025-04-03 10:36:11 +0530

    Added chat history

--- potato-gpt/main.py
+++ potato-gpt/main.py
@@ -2,9 +2,14 @@ from utils.chat import chat_with_gpt
 
 if __name__ == '__main__':
 	print("WELCOME TO POTATO-GPT")
+	chat_history = [{'role':'assistant','content':'You are a helpful assistant.'}]
 	user_input = input("USER : ")
+	chat_history.append({'role':'user', 'content':user_input})
 	while user_input != '/exit':
-		response = chat_with_gpt(user_input)
+		response = chat_with_gpt(chat_history)
+		chat_history.append({"role": 'assistant', "content": response.output_text})
 		print(f"POTATO-GPT : {response.output_text}")
 		user_input = input("USER : ")
+		chat_history.append({ "role": "user", "content": user_input })
+
 	print("THANK YOU FOR USING POTATO-GPT!")
--- potato-gpt/utils/chat.py
+++ potato-gpt/utils/chat.py
@@ -1,9 +1,9 @@
-from openai import OpenAI
+from openai import OpenAI, api_key
 client = OpenAI()
 
 def chat_with_gpt(user_input):
 	response = client.responses.create(
-    		model="gpt-4o",
+    		model="gpt-4o-mini",
     		input=str(user_input)
 	)
 	return response
