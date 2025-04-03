e9856510efc493c2b7db418fba93b0ac2d81838b
Raunak <raunakkaushik@gmail.com>
2025-04-03 10:58:33 +0530

    added personas

--- potato-gpt/main.py
+++ potato-gpt/main.py
@@ -2,7 +2,9 @@ from utils.chat import chat_with_gpt
 
 if __name__ == '__main__':
 	print("WELCOME TO POTATO-GPT")
-	chat_history = [{'role':'assistant','content':'You are a helpful assistant.'}]
+	persona = 'default'
+	system_prompt = open(f'prompts/{persona}_prompt.txt').read()
+	chat_history = [{'role':'assistant','content':system_prompt}]
 	user_input = input("USER : ")
 	chat_history.append({'role':'user', 'content':user_input})
 	while user_input != '/exit':
@@ -10,6 +12,28 @@ if __name__ == '__main__':
 		chat_history.append({"role": 'assistant', "content": response.output_text})
 		print(f"POTATO-GPT : {response.output_text}")
 		user_input = input("USER : ")
-		chat_history.append({ "role": "user", "content": user_input })
+		if user_input == '/funny':
+			system_prompt = open(f'prompts/{user_input[1:]}_prompt.txt').read()
+			chat_history.append({'role':'assistant', 'content':system_prompt})
+			user_input = input("USER : ")
+			chat_history.append({'role':'user', 'content':user_input})
+		elif user_input == '/wise':
+			system_prompt = open(f'prompts/{user_input[1:]}_prompt.txt').read()
+			chat_history.append({'role':'assistant', 'content':system_prompt})
+			user_input = input("USER : ")
+			chat_history.append({'role':'user', 'content':user_input})
+		elif user_input == '/depressed':
+			system_prompt = open(f'prompts/{user_input[1:]}_prompt.txt').read()
+			chat_history.append({'role':'assistant', 'content':system_prompt})
+			user_input = input("USER : ")
+			chat_history.append({'role':'user', 'content':user_input})
+		elif user_input == '/bully':
+			system_prompt = open(f'prompts/{user_input[1:]}_prompt.txt').read()
+			chat_history.append({'role':'assistant', 'content':system_prompt})
+			user_input = input("USER : ")
+			chat_history.append({'role':'user', 'content':user_input})
+		else:
+			pass
+		chat_history.append({ "role": "user", "content": user_input})
 
 	print("THANK YOU FOR USING POTATO-GPT!")
